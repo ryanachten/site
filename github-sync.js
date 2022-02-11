bf1cca5d3b1c5ef32a86a63006a9bc1263fa9e6d
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-02-12 11:38:33 +1300

    Add markdown postprocessing to github sync

--- github-sync.js
+++ github-sync.js
@@ -6,29 +6,64 @@ const fetch = require('node-fetch')
 const yaml = require('js-yaml')
 
 const GITHUB_URL = 'https://raw.githubusercontent.com/ryanachten'
-const CONTENT_DIR = 'content/projects';
+const CONTENT_DIR = 'content/projects'
 
-let projects;
+const getGithubUrl = (project, branch, path, markdown = false) =>
+  `https://github.com/ryanachten/${project}/blob/${branch}/${path}${
+    markdown ? '' : '?raw=true'
+  }`
+
+let projects
 try {
-  const yamlFile = yaml.load(fs.readFileSync(`./${CONTENT_DIR}/index.yml`, 'utf8'))
-  projects = yamlFile.projects;
+  const yamlFile = yaml.load(
+    fs.readFileSync(`./${CONTENT_DIR}/index.yml`, 'utf8')
+  )
+  projects = yamlFile.projects
 } catch (error) {
-  console.error("Error reading projects yaml file", error);
+  console.error('Error reading projects yaml file', error)
 }
 
 projects.forEach(async ({ name, branch = 'main' }) => {
-  const projectName = name.toLowerCase();
+  const projectName = name.toLowerCase()
   try {
     const res = await fetch(`${GITHUB_URL}/${projectName}/${branch}/README.md`)
     if (res.ok && res.body !== null) {
-      const writeStream = fs.createWriteStream(
-        `${CONTENT_DIR}/${projectName}.md`
-      )
+      // File data is provided in buffer chunks
+      // - we need to wait for all chunks to be loaded before postprocessing
       const body = res.body
-      await new Promise((resolve, reject) => {
-        body.pipe(writeStream)
-        body.on('error', reject)
-        writeStream.on('finish', resolve)
+      let rawReadMe = ''
+      body.on('data', (chunk) => {
+        const data = chunk.toString()
+        rawReadMe += data
+      })
+
+      body.on('finish', () => {
+        // Replace all local file instances in the markdown,
+        // and replace them with the versions hosted remotely
+
+        // regex looks for md links and files like: ](./abc.ext
+        const localFileRegExp = /\]\(\.\/[\w\/]+.[\w]+/g
+        const updatedReadMe = rawReadMe.replaceAll(localFileRegExp, (str) => {
+          const path = str.replace('](./', '')
+          const extension = path.substring(path.length - 3, path.length)
+          const isMarkdown = extension === '.md'
+          const newPath = getGithubUrl(projectName, branch, path, isMarkdown)
+          console.log('replacing', path, 'with', newPath)
+          return `${']('}${newPath}`
+        })
+        // Once we're done postprocessing, we can write the file to disk
+        fs.writeFile(
+          `${CONTENT_DIR}/${projectName}.md`,
+          updatedReadMe,
+          (error) => {
+            if (error) {
+              console.error(`Error writing file for ${projectName}`, error)
+            } else {
+              console.log('Completed writing', projectName, '\n')
+              console.log('******\n')
+            }
+          }
+        )
       })
     } else {
       console.error(`Error retrieving ${name}`, res.status)
