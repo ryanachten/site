7330793c1c89d8155c13f0141c1a876d51af5b05
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-02-13 10:56:23 +1300

    Refactor project detail into component

---
+++ components/ProjectDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="project-detail">
+    <h1>{{ content.title }}</h1>
+    <p>{{ content.description }}</p>
+    <nuxt-content :document="content" />
+  </article>
+</template>
+
+<script lang="ts">
+import { FetchReturn } from '@nuxt/content/types/query-builder'
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  props: {
+    content: {
+      type: Object as PropType<FetchReturn>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.project-detail {
+  img {
+    width: 100%;
+  }
+}
+</style>
