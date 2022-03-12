06786e7f8d384ce64bf55cfd1749280dc9239309
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-03-13 10:06:37 +1300

    Add domain to TF resources

--- infra/resources.tf
+++ infra/resources.tf
@@ -28,3 +28,20 @@ resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.ryanachten.id
   branch_name = "main"
 }
+
+resource "aws_amplify_domain_association" "ryanachten" {
+  app_id      = aws_amplify_app.ryanachten.id
+  domain_name = "ryanachten.com"
+
+  # https://ryanachten.com
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = ""
+  }
+
+  # https://www.ryanachten.com
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = "www"
+  }
+}
