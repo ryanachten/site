b495caf8f07615db89d6533125b9563e7adf1249
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-03-12 17:33:17 +1300

    Add nuxt transition and mobile filter menu

--- components/ProjectGrid.vue
+++ components/ProjectGrid.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-    <p class="project-grid__header">All projects</p>
-    <div class="project-grid__wrapper">
-      <ProjectTile
-        v-for="project in projects"
-        :key="project.name"
-        :project="project"
-      />
-    </div>
+  <div class="project-grid__wrapper">
+    <ProjectTile
+      v-for="project in projects"
+      :key="project.name"
+      :project="project"
+    />
   </div>
 </template>
 
@@ -26,12 +23,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.project-grid__header {
-  @include subheader;
-  font-size: $font-s;
-  margin: 0 0 $m 0;
-}
-
 .project-grid__wrapper {
   column-gap: $m;
   display: grid;
