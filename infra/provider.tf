52c40a97d1fe7bba2d00edeee8d57a4defcfeada
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-09-21 08:26:26 -0700

    feat: add project tag to provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,4 +11,10 @@ provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_access_key
+
+  default_tags {
+    tags = {
+      Project = "site"
+    }
+  }
 }
