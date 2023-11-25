e3b60a85b220c342b15e62a9df6877a570f37521
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2023-11-25 09:11:15 -0800

    fix: responsive table and pre styles

--- components/ProjectDetail.vue
+++ components/ProjectDetail.vue
@@ -30,5 +30,27 @@ export default Vue.extend({
   a {
     color: $black;
   }
+
+  pre[class*='language-'] {
+    @media screen and (max-width: $desktop) {
+      max-width: calc(100vw - 70px); // prevent overflow on mobile
+    }
+  }
+
+  table {
+    display: block;
+    overflow-x: auto;
+    text-align: left;
+
+    @media screen and (max-width: $desktop) {
+      max-width: calc(100vw - 40px); // prevent overflow on mobile
+    }
+
+    th,
+    td {
+      padding: $xs;
+      vertical-align: top;
+    }
+  }
 }
 </style>
